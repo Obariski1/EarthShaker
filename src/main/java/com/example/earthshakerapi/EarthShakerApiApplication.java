c83d3a1e9a62115295117eb1cd664ecfebf93496
Palermo Giovanni (IT-PTR-CEN2-YPT - Extern) <giovanni.palermo@sbb.ch>
2024-04-30 09:56:49 +0200

    initial commit

---
+++ src/main/java/com/example/earthshakerapi/EarthShakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.earthshakerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthShakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthShakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/earthshakerapi/EarthShakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthshakerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthShakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
