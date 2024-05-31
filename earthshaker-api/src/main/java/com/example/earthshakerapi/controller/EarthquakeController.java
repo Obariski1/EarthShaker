cd1e2fbdff1adf999684d2f5ca3793df5ca4c5e1
Elias Elmer <eel137373@stud.gibb.ch>
2024-05-31 23:29:28 +0200

    Erbebenfilter

--- earthshaker-api/src/main/java/com/example/earthshakerapi/controller/EarthquakeController.java
+++ earthshaker-api/src/main/java/com/example/earthshakerapi/controller/EarthquakeController.java
@@ -10,7 +10,7 @@ public class EarthquakeController {
 
     @GetMapping("/earthquakes")
     public ResponseEntity<String> getEarthquakes() {
-        String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+        String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
         return ResponseEntity.ok(result);
--- earthshaker-web/src/App.vue
+++ earthshaker-web/src/App.vue
@@ -1,71 +1,160 @@
 <template>
-  <GMapMap
-      :center="center"
-      :zoom="2"
-      map-type-id="terrain"
-      style="width: 100%; height: 100vh"
-  >
-    <GMapMarker
-        :key="marker.id"
-        v-for="marker in markers"
-        :position="marker.position"
-    />
-  </GMapMap>
+  <div>
+    <header>
+      <h1>EarthShaker</h1>
+    </header>
+    <div class="filters">
+      <label>Date:</label>
+      <input type="date" v-model="dateFilter">
+
+      <label>Tsunami:</label>
+      <input type="checkbox" v-model="tsunamiFilter">
+
+      <div>
+        <h2>Stärke</h2>
+        <label>&lt; 3</label>
+        <input type="checkbox" v-model="MagTo3">
+        <label>3 - 3.9</label>
+        <input type="checkbox" v-model="Mag3">
+        <label>4 - 4.9</label>
+        <input type="checkbox" v-model="Mag4">
+        <label>5 - 5.9</label>
+        <input type="checkbox" v-model="Mag5">
+        <label>6 - 6.9</label>
+        <input type="checkbox" v-model="Mag6">
+        <label>7+</label>
+        <input type="checkbox" v-model="Mag7plus">
+
+      </div>
+    </div>
+    <GMapMap :center="center" :zoom="2" map-type-id="terrain" style="width: 100%; height: 90vh">
+      <GMapMarker :key="marker.id" v-for="marker in filteredMarkers" :position="marker.position" />
+    </GMapMap>
+  </div>
 </template>
+
 <script>
 import axios from 'axios';
-
+import { ref, watch } from 'vue';
+import { lookUp } from 'geojson-places';
+const result = lookUp(41.652349, -4.728602);
+console.log(result);
 export default {
   name: 'App',
   data() {
     return {
       center: { lat: 51.093048, lng: 6.842120 },
       markers: [],
-      earthquakeData: null
+      earthquakeData: null,
+      dateFilter: null,
+      tsunamiFilter: null,
+      MagTo3: true,
+      Mag3: true,
+      Mag4: true,
+      Mag5: true,
+      Mag6: true,
+      Mag7plus: true,
+
     };
   },
   created() {
-    axios.get('http://localhost:8080/earthquakes')
+    axios.get('http://localhost:8082/earthquakes')
       .then(response => {
-        // Die Antwort vom Backend enthält die Erdbebendaten
         this.earthquakeData = response.data;
-
-        // Verarbeiten Sie die erhaltenen Daten und aktualisieren Sie die 'markers'-Daten im Frontend
         this.processEarthquakeData();
       })
       .catch(error => {
         console.error('Error fetching earthquake data:', error);
       });
+
+    // Überwache Änderungen im tsunamiFilter und aktualisiere die Komponente
+    watch(this.tsunamiFilter, (newVal) => {
+      this.$forceUpdate();
+    });
+  },
+  computed: {
+    filteredMarkers() {
+      if (!this.earthquakeData) return [];
+
+      return this.markers.filter(marker => {
+        var filterSatisfied
+        if (this.dateFilter) {
+          const date = new Date(marker.time);
+          const dateString = date.toISOString().split('T')[0];
+          if (dateString !== this.dateFilter) {
+            return false;
+          }
+        }
+
+      
+        if (this.tsunamiFilter && marker.tsunami !== 1) {
+          return false;
+        }
+
+        const magnitude = marker.magnitude;
+
+        // Check if the magnitude falls within any of the selected ranges
+        if ((this.MagTo3 && magnitude <= 3) ||
+          (this.Mag3 && magnitude >= 3 && magnitude <= 3.9) ||
+          (this.Mag4 && magnitude >= 4 && magnitude <= 4.9) ||
+          (this.Mag5 && magnitude >= 5 && magnitude <= 5.9) ||
+          (this.Mag6 && magnitude >= 6 && magnitude <= 6.9) ||
+          (this.Mag7plus && magnitude >= 7)) {
+          return true; // Include the marker if it falls within any selected range
+        }
+        else {
+          return false;
+        }
+      });
+    }
+
   },
   methods: {
     processEarthquakeData() {
-      console.log(this.earthquakeData)
-
       if (this.earthquakeData) {
-        // Extrahiere die Erdbeben aus den erhaltenen Daten
         const earthquakes = this.earthquakeData.features;
 
-        // Iteriere über jedes Erdbeben und konvertiere es in das gewünschte Marker-Format
         earthquakes.forEach(earthquake => {
           const id = earthquake.id;
           const coordinates = earthquake.geometry.coordinates;
           const latitude = coordinates[1];
           const longitude = coordinates[0];
+          const time = earthquake.properties.time;
+          const tsunami = earthquake.properties.tsunami;
 
-          // Erstelle den Marker im gewünschten Format
+          const magnitude = earthquake.properties.mag;
+
+
+
           const marker = {
             id: id,
             position: {
               lat: latitude,
               lng: longitude
-            }
+            },
+            time: time,
+            tsunami: tsunami,
+            magnitude: magnitude,
           };
-
-          // Füge den Marker dem 'markers'-Array hinzu
           this.markers.push(marker);
         });
       }
+    },
+    async performReverseGeocoding() {
+    try {
+      const result = await lookUp(41.652349, -4.728602);
+      console.log(result);
+      // Handle the result here
+    } catch (error) {
+      console.error('Error performing reverse geocoding:', error);
     }
+  }
   }
 };
 </script>
+
+<style>
+.filters {
+  margin-bottom: 20px;
+}
+</style>
